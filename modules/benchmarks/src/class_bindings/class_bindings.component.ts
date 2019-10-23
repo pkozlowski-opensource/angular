52a472366a035ee8b7ddce910f156604a20dcfb4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2019-10-23 15:30:32 +0200

    perf(ivy): class bindings benchmark

---
+++ modules/benchmarks/src/class_bindings/class_bindings.component.ts
@@ -0,0 +1,41 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'class-bindings',
+  template: `
+  <div>
+  <p>{{ msg }}</p>
+  <div *ngFor="let obj of list; let i = index" [title]="msg + i">
+    <span [class]="msg">{{ obj.text }}</span>
+    <span class="baz">one</span>
+    <span class="qux">two</span>
+    <div>
+      <span class="qux">three</span>
+      <span class="qux">four</span>
+      <span class="baz">five</span>
+      <div>
+        <span class="qux">six</span>
+        <span class="baz">seven</span>
+        <span [class]="msg">eight</span>
+      </div>
+    </div>
+  </div>
+</div>
+  `
+})
+export class ClassBindingsComponent {
+  msg = 'hello';
+  list = new Array(1000).fill(0).map((_, i) => ({i, text: 'foobar' + i}));
+
+  update() {
+    this.msg = this.msg === 'hello' ? 'bye' : 'hello';
+    this.list[0].text = this.msg;
+  }
+}
