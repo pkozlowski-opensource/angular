8a75c1043e25dd96141f63cb89fcb05b21f4e6b5
Miško Hevery <misko@hevery.com>
2020-01-17 15:30:21 -0800

    fixup! refactor(ivy): Switch styling to new reconcile algorithm (#34616)

--- tools/public_api_guard/core/core.d.ts
+++ tools/public_api_guard/core/core.d.ts
@@ -702,7 +702,7 @@ export declare function ɵɵattributeInterpolateV(attrName: string, values: any[
 
 export declare function ɵɵclassMap(classes: {
     [className: string]: any;
-} | string | null): typeof ɵɵclassMap;
+} | string | null): void;
 
 export declare function ɵɵclassMapInterpolate1(prefix: string, v0: any, suffix: string): void;
 
@@ -1033,7 +1033,7 @@ export declare function ɵɵstaticViewQuery<T>(predicate: Type<any> | string[], 
 
 export declare function ɵɵstyleMap(styles: {
     [styleName: string]: any;
-} | string | null): typeof ɵɵstyleMap;
+} | string | null): void;
 
 export declare function ɵɵstyleProp(prop: string, value: string | number | SafeValue | null, suffix?: string | null): typeof ɵɵstyleProp;
 
